373dc3cc5d4faf3b26501d3679d42721b1dff42b
Araceli Martinez <araceli_martinez94@hotmail.com>
2023-03-06 13:55:23 -0300

    Stage merge

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,6 +3,7 @@ class RentalsController < ApplicationController
 
   def new
     @rental = Rental.new
+    authorize @rental
   end
 
   def create
@@ -12,6 +13,7 @@ class RentalsController < ApplicationController
     if !@rental.end_date.nil? && !@rental.start_date.nil?
       @rental.total_price = @accommodation.price * (@rental.end_date - @rental.start_date)
     end
+    authorize @rental
     if @rental.save
       # REDIRECT TO My_Rentals when we have it
       redirect_to bookings_rentals_path
@@ -25,6 +27,7 @@ class RentalsController < ApplicationController
   end
 
   def destroy
+    authorize @rental
     @booking = Rental.find(params[:id])
     @booking.destroy
     redirect_to bookings_rentals_path, notice: "Your booking was canceled successfully."
