ece4f4939d6cdc193f1a4e81a2988a0eba2350fe
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-03-07 14:35:36 -0300

    stage merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_192016) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_144547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_192016) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "planet"
     t.index ["user_id"], name: "index_accommodations_on_user_id"
   end
 
